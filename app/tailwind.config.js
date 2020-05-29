ef06bcc6f19a400530e82103a15a70bef4bb3faa
kissmygritts <kissmygritts08@gmail.com>
2020-05-29 16:04:59 -0700

    App init commit, bootstrap Nuxt project

---
+++ app/tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {},
+  variants: {},
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
+}
