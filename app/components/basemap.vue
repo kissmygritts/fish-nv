b04053f42f0885786b333977928040e155540651
kissmygritts <kissmygritts08@gmail.com>
2020-05-29 19:27:58 -0700

    App: map with markes over fishable waters

---
+++ app/components/basemap.vue
@@ -0,0 +1,41 @@
+<template>
+  <client-only>
+    <l-map>
+      <l-tile-layer :url="url" />
+      <marker-with-tooltip
+        v-for="(water, index) in fishableWaters"
+        :key="index"
+        :lat-lng="water.coordinates"
+      >
+        <p>{{ water.water_name }}</p>
+      </marker-with-tooltip>
+    </l-map>
+  </client-only>
+</template>
+
+<script>
+import MarkerWithTooltip from '@/components/marker-with-tooltip.vue'
+
+export default {
+  name: 'Basemap',
+
+  components: { MarkerWithTooltip },
+
+  props: {
+    fishableWaters: {
+      type: Array,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      url: 'https://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      mapPoints: [
+        [47.4132, -1.2914],
+        [0, 0]
+      ]
+    }
+  }
+}
+</script>
---
+++ app/components/marker-with-tooltip.vue
@@ -0,0 +1,29 @@
+<template>
+  <l-marker :lat-lng="latLng" :color="color">
+    <l-tooltip><slot>tooltip content</slot></l-tooltip>
+  </l-marker>
+</template>
+
+<script>
+export default {
+  name: 'MarkerWithTooltip',
+
+  props: {
+    latLng: {
+      type: Array,
+      require: true,
+      default () {
+        return [0, 0]
+      }
+    },
+    color: {
+      type: String,
+      default: '#6958d0'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
