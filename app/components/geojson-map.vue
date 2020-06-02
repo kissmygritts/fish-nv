a249726cf2bbedd4c2289c030c33182c9db2f07f
kissmygritts <kissmygritts08@gmail.com>
2020-06-01 22:17:57 -0700

    App: geojson map on fishable-waters/:id route

---
+++ app/components/geojson-map.vue
@@ -0,0 +1,37 @@
+/* eslint-disable */
+<template>
+  <client-only>
+    <l-map ref="map" :center="center" :zoom="zoom" @ready="setMapBounds()">
+      <l-tile-layer :url="url" />
+      <l-geo-json ref="geojson" :geojson="geojson" />
+    </l-map>
+  </client-only>
+</template>
+
+<script>
+export default {
+  name: 'GeoJsonMap',
+  props: {
+    geojson: {
+      type: Object,
+      default () {
+        return null
+      }
+    }
+  },
+
+  data () {
+    return {
+      url: 'https://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      center: [38.64954285997146, -116.77592011899117],
+      zoom: 6
+    }
+  },
+
+  methods: {
+    setMapBounds () {
+      this.$refs.map.setBounds(this.$refs.geojson.getBounds())
+    }
+  }
+}
+</script>
--- app/pages/fishable-waters/_id.vue
+++ app/pages/fishable-waters/_id.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="text-gray-900">
-    <header class="p-8">
+    <header class="px-2 py-6 md:px-4 md:py-8 lg:px-8">
       <h1 class="text-4xl">
         {{ fishableWater.water_name }}
       </h1>
     </header>
 
     <div class="flex flex-wrap w-100">
-      <div id="map" class="flex-grow bg-blue-400">
-        MAP
+      <div id="map" class="w-full lg:w-3/5 bg-blue-400">
+        <geo-json-map :geojson="parsedGeoJson" />
       </div>
-      <div class="w-2/5 bg-green-400 py-8 px-6">
+      <div class="w-full lg:w-2/5 bg-green-400 py-8 px-6">
         <table class="text-xl table-auto min-w-full">
           <tr>
             <td class="font-bold tracking-wide">
@@ -68,13 +68,24 @@
 
 <script>
 import axios from 'axios'
+import GeoJsonMap from '@/components/geojson-map.vue'
 
 export default {
+  components: {
+    GeoJsonMap
+  },
+
   async asyncData ({ params }) {
     const url = `http://localhost:3030/fishable-waters/${params.id}`
     const res = await axios.get(url)
     return {
       fishableWater: res.data
+    }
+  },
+
+  computed: {
+    parsedGeoJson () {
+      return JSON.parse(this.fishableWater.geojson)
     }
   }
 }
