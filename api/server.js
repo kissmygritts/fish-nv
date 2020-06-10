ab72c4072cfdd70c1a641f3cd27999bf03ec2665
kissmygritts <kissmygritts08@gmail.com>
2020-06-10 09:37:38 -0700

    Api: Fastify listen from 0.0.0.0 in production from documentation

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ routes.forEach((route) => {
 
 const start = async () => {
   try {
-    await fastify.listen(3030)
+    await fastify.listen({ port: 8000, host: '0.0.0.0' })
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
