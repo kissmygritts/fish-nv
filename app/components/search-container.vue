8cb30b1fd5f8c807b2d28d9399fb7800368f72d2
kissmygritts <kissmygritts08@gmail.com>
2020-06-18 13:23:20 -0700

    App: moved search to its own container

---
+++ app/components/search-container.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="text-gray-700">
+    <!-- water type -->
+    <div class="py-2">
+      <label for="water-type">Select Water Type</label>
+      <div class="relative inline-block w-full mt-1">
+        <select
+          id="waterType"
+          v-model="waterType.selected"
+          name="water-type"
+          class="block appearance-none w-full bg-white px-4 py-3 pr-8 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline"
+          @change="selected"
+        >
+          <option value="" />
+          <option value="creek">
+            creek
+          </option>
+          <option value="stream">
+            stream
+          </option>
+          <option value="river">
+            river
+          </option>
+          <option value="pond">
+            pond
+          </option>
+          <option value="lake">
+            lake
+          </option>
+          <option value="reservoir">
+            reservoir
+          </option>
+        </select>
+        <div
+          class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+        >
+          <svg
+            class="w-4 h-4"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M19 9l-7 7-7-7" />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <!-- species input -->
+    <div class="py-2">
+      <label for="species">Select Species</label>
+      <div class="relative inline-block w-full mt-1">
+        <select
+          id="species"
+          v-model="species.selected"
+          name="species"
+          class="block appearance-none w-full bg-white px-4 py-3 pr-8 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline"
+          @change="selected"
+        >
+          <option
+            v-for="(fish, index) in species.options"
+            :key="index"
+            :value="fish"
+          >
+            {{ fish }}
+          </option>
+        </select>
+        <div
+          class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+        >
+          <svg
+            class="w-4 h-4"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M19 9l-7 7-7-7" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchContainer',
+
+  data () {
+    return {
+      waterType: {
+        selected: null
+      },
+      species: {
+        selected: null,
+        options: [
+          '',
+          'arctic grayling',
+          'black crappie',
+          'bluegill sunfish',
+          'bonneville cutthoat trout',
+          'bowcutt trout',
+          'brook trout',
+          'brown trout',
+          'bull trout',
+          'bullhead catfish',
+          'carp',
+          'channel catfish',
+          'crappie',
+          'cutthroat',
+          'golden trout',
+          'green sunfish',
+          'kokanee salmon',
+          'lahontan cutthroat trout',
+          'largemouth bass',
+          'mackinaw trout',
+          'northern pike',
+          'pumpkinseed',
+          'rainbow trout',
+          'redband trout',
+          'redear sunfish',
+          'sacramento perch',
+          'silver salmon',
+          'smallmouth bass',
+          'spotted bass',
+          'striped bass',
+          'tiger musky',
+          'tiger trout',
+          'walleye',
+          'white bass',
+          'white catfish',
+          'white crappie',
+          'whitefish',
+          'wiper',
+          'yellow perch',
+          'yellowstone cutthroat trout'
+        ]
+      }
+    }
+  },
+
+  computed: {
+    payload () {
+      const speciesString = this.species.selected
+        ? Array.isArray(this.species)
+          ? this.species.selected.join(' or ')
+          : this.species.selected
+        : ''
+      const waterString = this.waterType.selected ?? ''
+
+      const s = [speciesString, waterString]
+        .filter(str => str !== '')
+        .join()
+
+      return {
+        s: s === '' ? null : s
+      }
+    }
+  },
+
+  methods: {
+    selected (e) {
+      // if (e.target.value === '') {
+      //   /* eslint-disable-next-line */
+      //   console.log(e.target.id)
+      //   this[e.target.id].selected = null
+      // }
+      this.$emit('selected', {
+        params: this.payload
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,46 +1,22 @@
 <template>
   <div class="flex h-screen w-screen">
     <main id="map" class="bg-blue-400 flex-grow">
-      <!-- <l-map :center="center" :zoom="zoom">
-        <l-tile-layer :url="url" />
-        <geo-json-layer
-          :geojson="fishableWaters.geojson"
-          :enable-tooltip="true"
-          @click:feature="navigateTo"
-        />
-      </l-map> -->
-      <p class="text-center">
-        map
-      </p>
+      <client-only>
+        <l-map :center="center" :zoom="zoom">
+          <l-tile-layer :url="url" />
+        </l-map>
+      </client-only>
+
+      <!-- <geo-json-layer
+        :geojson="fishableWaters.geojson"
+        :enable-tooltip="true"
+        @click:feature="navigateTo"
+      /> -->
     </main>
 
     <aside class="bg-gray-100 w-1/4 flex flex-col px-8 overflow-y-auto">
-      <!-- search box -->
-      <div class="relative py-4">
-        <div
-          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
-        >
-          <svg
-            class="w-5 h-5 text-gray-500"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-          </svg>
-        </div>
-        <input
-          id="search"
-          v-model="search.query"
-          type="text"
-          class="block w-full pl-10 py-3 text-gray-800 placeholder-gray-500 rounded-md shadow-sm focus:outline-none focus:shadow-outline"
-          placeholder="Search..."
-          @keyup.enter="searchFishableWaters"
-        >
-      </div>
+      <!-- search container -->
+      <search-container @selected="doSomething" />
       <!-- results -->
       <div class="w-full mt-2">
         <div v-if="hasSearchResults">
@@ -71,45 +47,38 @@
 
 <script>
 // import GeoJsonLayer from '@/components/elements/geojson-layer.vue'
+import SearchContainer from '@/components/search-container'
 
 export default {
-  // components: { GeoJsonLayer },
-
-  async asyncData ({ $axios }) {
-    const res = await $axios.get('/api/geojson/fishable_waters?columns=water_name,id')
-
-    return {
-      fishableWaters: res.data
-    }
+  components: {
+    // GeoJsonLayer,
+    SearchContainer
   },
+
+  // async asyncData ({ $axios }) {
+  //   const res = await $axios.get('/api/geojson/fishable_waters?columns=water_name,id')
+
+  //   return {
+  //     fishableWaters: res.data
+  //   }
+  // },
 
   data () {
     return {
-      search: {
-        query: null,
-        loading: false,
-        results: null
-      },
       url: 'https://{s}.tile.osm.org/{z}/{x}/{y}.png',
       center: [38.64954285997146, -116.77592011899117],
-      zoom: 6
+      zoom: 6,
+      search: {
+        loading: false,
+        results: null,
+        params: null
+      }
     }
   },
 
   computed: {
     hasSearchResults () {
       return !!this.search.results
-    },
-
-    searchUrl () {
-      if (this.search.query) {
-        let s = this.search.query
-          .replaceAll(/\s*[,;:.-_]\s*/g, ',')
-        s = new URLSearchParams({ s })
-        return `/api/fishable-waters?${s.toString()}`
-      } else {
-        return ''
-      }
     }
   },
 
@@ -118,6 +87,28 @@ export default {
       this.$router.push({
         path: `fishable-waters/${feature.id}`
       })
+    },
+
+    /* eslint-disable-next-line */
+    async doSomething ({ params }) {
+      // set search.params -- probably not needed
+      this.search.params = params
+
+      // construct the query
+      const qs = new URLSearchParams()
+      if (params.s) {
+        qs.append('s', params.s)
+      }
+      const url = `/api/fishable-waters?${qs.toString()}`
+
+      /* eslint-disable-next-line */
+      console.log(url)
+
+      // start query to search
+      this.search.loading = true
+      const res = await this.$axios.get(url)
+      this.search.results = res.data
+      this.search.loading = false
     },
 
     async searchFishableWaters () {
