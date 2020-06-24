dc9d0f2478b1ac78a84bdfb799d22f92fd13256d
kissmygritts <kissmygritts08@gmail.com>
2020-06-24 16:00:26 -0700

    Api: update fishable-waters/:id route, doesnt return fish entries

--- api/db/sql/fishable-waters/get-by-id.sql
+++ api/db/sql/fishable-waters/get-by-id.sql
@@ -1,2 +1,21 @@
-SELECT * FROM fishable_waters_route
-WHERE id = $<id>;  
+-- SELECT * FROM fishable_waters_route
+-- WHERE id = $<id>;  
+
+SELECT
+  fw.id,
+  fw.water_name,
+  fw.label,
+  fw.region,
+  fw.county,
+  species.species_arr AS species,
+  st_asGeoJSON(geom, 5) as geojson
+FROM fishable_waters AS fw
+  JOIN (
+    SELECT
+      species_water_joiner.water_id,
+      jsonb_agg(species.species) as species_arr
+    FROM species
+      JOIN species_water_joiner ON species.id = species_water_joiner.species_id
+    GROUP BY species_water_joiner.water_id
+  ) AS species ON fw.id = species.water_id
+WHERE id = $<id>
